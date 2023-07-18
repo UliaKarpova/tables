56bf3bfd088f12fd239156bcc02baa816f808141
Ulia Karpova <chelovek-e-95@mail.ru>
2023-07-18 12:08:41 +0300

    fix-Case&Type

--- src/redux/findData.ts
+++ src/redux/findData.ts
@@ -6,9 +6,9 @@ export default function findData(data: DataType[], str: string) {
     let myItem;
     for (let key in item) {
       // приводим значение свойства к строке
-      let value = item[key].toString();
+      let value = item[key].toString().toLowerCase();
       // проверяем, есть ли в значении str
-      if (value.includes(str)) {
+      if (value.includes(str.toLowerCase())) {
         myItem = item;
       };
     }
--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,8 +1,9 @@
 import findData from './findData';
 import { DataType } from '../types';
+import { RootState } from './store';
 
 // ищем данные в по переданной строке
-export const findDataReducer = (state: any, action: any) => {
+export const findDataReducer = (state: RootState, action: any) => {
   const newData: DataType[] = findData(state[action.payload.id], action.payload.searchQuery);
   return {
     ...state,
@@ -11,7 +12,7 @@ export const findDataReducer = (state: any, action: any) => {
 }
 
 // сохраняем в стор данные из исходного массива
-export const addDataReducer = (state: any, action: any) => {
+export const addDataReducer = (state: RootState, action: any) => {
   const { data, id } = action.payload;
   return {
     ...state,
